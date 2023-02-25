c1b93d4be95a8a1cc72f0bc68313dc5873a240d0
sagitovramil <sagitovramil86@gmail.com>
2023-02-25 21:23:21 +0500

    7

--- src/Interfaces/Test.java
+++ src/Interfaces/Test.java
@@ -5,6 +5,10 @@ public class Test {
     public static void main(String[] args){
         Animal animal1 = new Animal(1);
         Person person1 = new Person("Ramil");
-
+        outputInfo(animal1);
+        outputInfo(person1);
+    }
+    public static void outputInfo(Info info){
+        info.showInfo();
     }
 }
